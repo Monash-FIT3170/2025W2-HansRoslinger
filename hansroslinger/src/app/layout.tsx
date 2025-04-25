35313d57707c4d7c0fe6ac8b6b67a91fe855a686
preedy2five <maxlay89@gmail.com>
2025-04-25 10:58:33 +0000

    Refactored root layout to render ReturnToDashboard component. Also removed gap between logo and yubi title.

--- hansroslinger/src/app/layout.tsx
+++ hansroslinger/src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import Image from 'next/image';
 import Link from 'next/link';
 import { ReactNode } from 'react';
+import ReturnToDashboard from '@/components/ReturnToDashboard';
 
 export const metadata = {
   title: 'Yubi',
@@ -12,19 +13,21 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className="h-full">
-    <body className="h-full">
-      <div className="flex flex-col h-full bg-[#fffcee]">
-        <header className="h-20 px-6 flex items-center">
-        <Link href="/" className="flex items-center gap-3">
-              <Image src="/yubi-logo.png" alt="Yubi Logo" width={65} height={65} />
-              <span className="text-4xl font-bold text-[#1e1e1e]">Yubi</span>
+      <body className="h-full">
+        <div className="flex flex-col h-full bg-[#fffcee]">
+          <header className="h-20 px-6 flex items-center justify-between">
+            <Link href="/" className="flex items-center">
+                <Image src="/yubi-logo.png" alt="Yubi Logo" width={65} height={65} />
+                <span className="text-4xl font-bold text-[#1e1e1e]">Yubi</span>
             </Link>
-        </header>
-        <main className="flex-1 overflow-y-auto lg:overflow-hidden">
-          {children}
-        </main>
-      </div>
-    </body>
+            {/* Conditionally rendered return button */}
+            <ReturnToDashboard />
+          </header>
+          <main className="flex-1 overflow-y-auto lg:overflow-hidden">
+            {children}
+          </main>
+        </div>
+      </body>
     </html>
   );
 }
