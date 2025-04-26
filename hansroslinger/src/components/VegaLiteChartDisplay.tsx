2230bad3cea9bc45fff56a5c9f1d2abfd8c2258f
StephSS14 <stephanie.sisilia@gmail.com>
2025-04-26 07:51:39 +0000

    feat(UI/UX): Added json display using vegalite

---
+++ hansroslinger/src/components/VegaLiteChartDisplay.tsx
@@ -0,0 +1,28 @@
+'use client';
+import React, { useEffect, useRef } from 'react';
+import { UploadProp } from 'types/application';
+import embed from 'vega-embed';
+
+type VegaLiteChartDisplayProp = {
+  data: UploadProp
+}
+
+const VegaLiteChartDisplay = ({data}: VegaLiteChartDisplayProp) => {
+  const chartRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      embed(chartRef.current, data.src, {
+        actions: false, // disables the action menu
+      });
+    }
+  }, [data.src]);
+
+  return (
+    <div className="w-full h-full">
+      <div ref={chartRef} className="w-full h-full" />
+    </div>
+  )
+};
+
+export default VegaLiteChartDisplay;
