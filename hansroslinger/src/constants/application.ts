1e8a84932de520631b0d33d9849face04b18a3c2
StephSS14 <stephanie.sisilia@gmail.com>
2025-04-26 07:51:39 +0000

    feat(UI/UX): Added json display using vegalite

--- hansroslinger/src/constants/application.ts
+++ hansroslinger/src/constants/application.ts
@@ -1,1 +1,4 @@
-export const ACCEPTED_FILE_TYPES = ["png, json"]
+export const FILE_TYPE_PNG = 'png'
+export const FILE_TYPE_JSON = 'json'
+
+export const ACCEPTED_FILE_TYPES = [FILE_TYPE_PNG, FILE_TYPE_JSON]
