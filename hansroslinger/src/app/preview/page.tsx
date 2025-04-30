ad67b12d050b72bf83241cfc7fbb5e1f3cdf4db7
StephSS14 <stephanie.sisilia@gmail.com>
2025-04-30 00:55:52 +0000

    Run linting

--- hansroslinger/src/app/preview/page.tsx
+++ hansroslinger/src/app/preview/page.tsx
@@ -5,8 +5,10 @@ import CameraFeed from "./CameraFeed";
 const Preview = () => {
   return (
     <div className="flex justify-center px-4 sm:px-8">
-      <div className="w-full max-w-full sm:max-w-2xl md:max-w-4xl lg:max-w-6xl xl:max-w-7xl 
-        aspect-video border-2 border-black overflow-hidden bg-black">
+      <div
+        className="w-full max-w-full sm:max-w-2xl md:max-w-4xl lg:max-w-6xl xl:max-w-7xl 
+        aspect-video border-2 border-black overflow-hidden bg-black"
+      >
         <CameraFeed />
       </div>
     </div>
