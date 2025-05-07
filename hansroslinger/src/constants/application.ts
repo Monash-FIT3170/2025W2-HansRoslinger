8b5257a6673304fcc502bba82b2ad12ed35e115a
StephSS14 <stephanie.sisilia@gmail.com>
2025-05-07 14:10:59 +0000

    Feature(UI/UX): Save selected uploads to local storage using persists

--- hansroslinger/src/constants/application.ts
+++ hansroslinger/src/constants/application.ts
@@ -2,3 +2,5 @@ export const FILE_TYPE_PNG = "png";
 export const FILE_TYPE_JSON = "json";
 
 export const ACCEPTED_FILE_TYPES = [FILE_TYPE_PNG, FILE_TYPE_JSON];
+
+export const LOCAL_STORAGE_KEY_SELECTED_UPLOAD = "selectedUploadKey";
