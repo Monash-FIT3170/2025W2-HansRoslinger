050c759fd0307ddc7f88bf0d8f3bd71f4c81085d
StephSS14 <stephanie.sisilia@gmail.com>
2025-05-07 14:10:59 +0000

    Feature(UI/UX): Save selected uploads to local storage using persists

--- hansroslinger/src/app/store/uploadsSlice.ts
+++ hansroslinger/src/app/store/uploadsSlice.ts
@@ -1,5 +1,7 @@
+import { LOCAL_STORAGE_KEY_SELECTED_UPLOAD } from "constants/application";
 import { UploadProp, Uploads } from "types/application";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type SelectedUploadState = {
   selectedUploads: Uploads;
@@ -10,29 +12,39 @@ type SelectedUploadState = {
   getSelectedUpload: (assetId: string) => UploadProp | undefined;
 };
 
-export const useUploadStore = create<SelectedUploadState>((set, get) => ({
-  selectedUploads: {},
+export const useUploadStore = create<SelectedUploadState>()(
+  persist(
+    (set, get) => ({
+      selectedUploads: {},
 
-  setSelectedUpload: (assetId, data) =>
-    set((state) => ({
-      selectedUploads: {
-        ...state.selectedUploads,
-        [assetId]: data,
-      },
-    })),
+      setSelectedUpload: (assetId, data) =>
+        set((state) => ({
+          selectedUploads: {
+            ...state.selectedUploads,
+            [assetId]: data,
+          },
+        })),
 
-  removeSelectedUpload: (assetId) =>
-    set((state) => ({
-      // use formEntries to change list of [assetId, uploadProp] into an object
-      selectedUploads: Object.fromEntries(
-        // .entries returns a list
-        Object.entries(state.selectedUploads).filter(
-          ([key]) => key !== assetId,
-        ),
-      ),
-    })),
+      removeSelectedUpload: (assetId) =>
+        set((state) => ({
+          // use formEntries to change list of [assetId, uploadProp] into an object
+          selectedUploads: Object.fromEntries(
+            // .entries returns a list
+            Object.entries(state.selectedUploads).filter(
+              ([key]) => key !== assetId,
+            ),
+          ),
+        })),
 
-  clearSelectedUploads: () => set({ selectedUploads: {} }),
+      clearSelectedUploads: () => set({ selectedUploads: {} }),
 
-  getSelectedUpload: (assetId) => get().selectedUploads[assetId],
-}));
+      getSelectedUpload: (assetId) => get().selectedUploads[assetId],
+    }),
+    {
+      // For persists
+      // zustand persists default to storing it in localstorage
+      // name is local storage key
+      name: LOCAL_STORAGE_KEY_SELECTED_UPLOAD,
+    },
+  ),
+);
