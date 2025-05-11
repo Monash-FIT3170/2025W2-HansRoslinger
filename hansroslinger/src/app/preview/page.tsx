d85b9871c85b1dedc13806213828f23d73b14c78
Joshua Cui <xcui0015@student.monash.edu>
2025-05-11 13:37:32 +0000

    feat(UI): set up the konva.js canvas

--- hansroslinger/src/app/preview/page.tsx
+++ hansroslinger/src/app/preview/page.tsx
@@ -1,17 +1,21 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import CameraFeed from "./CameraFeed";
 
+const KonvaOverlay = dynamic(() => import("@/components/KonvaOverlay"), {
+  ssr: false,
+});
+
 const Preview = () => {
-  // Example of getting all the selected uploads
-  // const selectedUploads = useUploadStore((state) => state.uploads);
   return (
     <div className="flex justify-center px-4 sm:px-8">
       <div
-        className="w-full max-w-full sm:max-w-2xl md:max-w-4xl lg:max-w-6xl xl:max-w-7xl 
+        className="relative w-full max-w-full sm:max-w-2xl md:max-w-4xl lg:max-w-6xl xl:max-w-7xl 
         aspect-video border-2 border-black overflow-hidden bg-black"
       >
         <CameraFeed />
+        <KonvaOverlay />
       </div>
     </div>
   );
