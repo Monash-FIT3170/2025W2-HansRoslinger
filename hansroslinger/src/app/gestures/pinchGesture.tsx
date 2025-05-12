bb18970dae9a9e577d52a0a5b28f7cb9e6170b3c
rjaggi3 <rohanjaggi2003@gmail.com>
2025-05-12 08:33:46 +0000

    Cleaned up code for pitch gesture

--- hansroslinger/src/app/gestures/pinchGesture.tsx
+++ hansroslinger/src/app/gestures/pinchGesture.tsx
@@ -1,23 +1,18 @@
 import fp from 'fingerpose';
-import { IFingerPose, IGestureDescription } from '../types';
-
-const { Finger, FingerCurl, FingerDirection, GestureDescription } = fp as IFingerPose;
-
-// Define pinching gesture 🤏
-const pinchGesture = new GestureDescription('pinching') as IGestureDescription;
+const pinchGesture = new fp.GestureDescription('pinch');
 
 // Thumb specifications
-pinchGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0);
-pinchGesture.addDirection(Finger.Thumb, FingerDirection.HorizontalRight, 0.5);
-pinchGesture.addDirection(Finger.Thumb, FingerDirection.DiagonalUpRight, 0.5);
+pinchGesture.addCurl(fp.Finger.Thumb, fp.FingerCurl.NoCurl, 1.0);
+pinchGesture.addDirection(fp.Finger.Thumb, fp.FingerDirection.HorizontalRight, 0.5);
+pinchGesture.addDirection(fp.Finger.Thumb, fp.FingerDirection.DiagonalUpRight, 0.5);
 
 // Index finger specifications
-pinchGesture.addCurl(Finger.Index, FingerCurl.HalfCurl, 1.0);
+pinchGesture.addCurl(fp.Finger.Index, fp.FingerCurl.HalfCurl, 1.0);
 
 // Remaining fingers should be curled
-for (const finger of [Finger.Middle, Finger.Ring, Finger.Pinky]) {
-    pinchGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
-    pinchGesture.addCurl(finger, FingerCurl.HalfCurl, 0.9);
+for (const finger of [fp.Finger.Middle, fp.Finger.Ring, fp.Finger.Pinky]) {
+    pinchGesture.addCurl(finger, fp.FingerCurl.FullCurl, 1.0);
+    pinchGesture.addCurl(finger, fp.FingerCurl.HalfCurl, 0.9);
 }
 
 export default pinchGesture;
