935115c26f9ce4f67e3c4066ce39019dffb2246f
rjaggi3 <rohanjaggi2003@gmail.com>
2025-05-21 11:51:46 +0000

    Cleaned up code in handRecognition

--- hansroslinger/src/app/handRecognition.ts
+++ hansroslinger/src/app/handRecognition.ts
@@ -1,53 +1,47 @@
 import { GestureRecognizer, HandLandmarker, FilesetResolver, DrawingUtils } from "@mediapipe/tasks-vision";
-import * as fp from 'fingerpose';
-// TODO, get document element for video on page.ts
 
 
 let handLandmarker: HandLandmarker |undefined = undefined;
 let gestureRecognizer: GestureRecognizer;
 const runningMode: "VIDEO" | "IMAGE" = "VIDEO";
-const GE: fp.GestureEstimator = new fp.GestureEstimator([fp.Gestures.ThumbsUpGesture, fp.Gestures.VictoryGesture]);
-// let enableWebcamButton: HTMLButtonElement;
-// let webcamRunning: boolean = false;
 
-// Before we can use HandLandmarker class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment to
-// get everything needed to run.
 export const createHandLandmarker = async () => {
   const vision = await FilesetResolver.forVisionTasks(
-    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasms"
   );
   handLandmarker = await HandLandmarker.createFromOptions(vision, {
     baseOptions: {
       modelAssetPath: "/models/hand_landmarker.task",
-      delegate: "CPU"
+      delegate: "GPU"
     },
     runningMode: runningMode,
     numHands: 2
   });
-  console.log("loaded hand landmark mediapipe")
   return handLandmarker;
 };
 
+
 export const createGestureRecognizer = async () => {
   const vision = await FilesetResolver.forVisionTasks(
-    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
   );
   gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
     baseOptions: {
-      modelAssetPath:
-        "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+      modelAssetPath: "https://storage.googleapis.com/mediapipe-tasks/gesture_recognizer/gesture_recognizer.task",
       delegate: "GPU"
     },
     runningMode: runningMode
   });
   console.log("gesture recognise mediapipe")
+  
 };
-createGestureRecognizer();
+await createGestureRecognizer();
+
 
 export const HandRecogniser = async (video: HTMLVideoElement, canvas: HTMLCanvasElement ) =>{
-   console.log("HandRecogniser started");
-
+  console.log("HandRecogniser started");
+  //await createHandLandmarker();
+  
   const canvasCtx = canvas.getContext("2d");
   const drawingUtils = new DrawingUtils(canvasCtx!);
 
@@ -60,19 +54,21 @@ export const HandRecogniser = async (video: HTMLVideoElement, canvas: HTMLCanvas
     const startTimeMs = performance.now();
     if (lastVideoTime !== video.currentTime) {
       lastVideoTime = video.currentTime;
-      const results = await handLandmarker.detectForVideo(video, startTimeMs);
-      const gestureRecognitionResult = gestureRecognizer.recognizeForVideo(video,startTimeMs);
-
+      //const results = await handLandmarker.detectForVideo(video, startTimeMs);
+      const gestureRecognitionResult = await gestureRecognizer.recognizeForVideo(video,startTimeMs);
 
       canvasCtx!.save();
       canvasCtx!.scale(-1, 1); 
       canvasCtx!.translate(-canvas.width, 0);
       canvasCtx!.clearRect(0, 0, canvas.width, canvas.height);
       
-      console.log(results)
-      for (let i =0; i<results.worldLandmarks.length; i++) {
+      console.log(gestureRecognitionResult)
+      for (let i =0; i<gestureRecognitionResult.worldLandmarks.length; i++) {
         
-        const landmarks = results.landmarks[i];
+        const landmarks = gestureRecognitionResult.landmarks[i];
+        //const isPinching = pinch(landmarks);
+        //console.log(results);
+
         console.log("Hand landmarks:", landmarks);
         drawingUtils.drawConnectors(landmarks, HandLandmarker.HAND_CONNECTIONS, {
           color: "#00FF00",
