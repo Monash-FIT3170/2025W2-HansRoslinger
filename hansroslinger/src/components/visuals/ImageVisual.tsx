d4df88a2ffa78b4d1c2abb0d522c32cef3731220
StephSS14 <stephanie.sisilia@gmail.com>
2025-05-21 01:27:41 +0000

    feature (UI/UX): image visual size to follow original file size

--- hansroslinger/src/components/visuals/ImageVisual.tsx
+++ hansroslinger/src/components/visuals/ImageVisual.tsx
@@ -3,6 +3,7 @@
 import { Image as KonvaImage } from "react-konva";
 import React, { useEffect, useState } from "react";
 import { Visual } from "types/application";
+import { useVisualStore } from "store/visualsSlice";
 
 type ImageVisualProp = {
   id: string;
@@ -11,14 +12,23 @@ type ImageVisualProp = {
 
 const ImageVisual = ({ id, visual }: ImageVisualProp) => {
   const [image, setImage] = useState<HTMLImageElement | null>(null);
+  const setVisualSize = useVisualStore((state) => state.setVisualSize);
 
   useEffect(() => {
+    
     const img = new window.Image();
     img.src = visual.uploadData.src;
     img.onload = () => {
       setImage(img);
+      setVisualSize(id, {
+      width: img.naturalWidth,
+      height: img.naturalHeight,
+    })
     };
-  }, [visual]);
+
+  }, [id, visual.uploadData.src, setVisualSize]);
+
+  console.log(visual)
 
   return (
     <>
@@ -30,6 +40,8 @@ const ImageVisual = ({ id, visual }: ImageVisualProp) => {
           y={visual.position.y}
           width={visual.size.width}
           height={visual.size.height}
+          stroke="red"
+          strokeWidth={5}
         />
       )}
     </>
