1611802c810a43812e5beaee3035e2ad838978f3
StephSS14 <stephanie.sisilia@gmail.com>
2025-05-21 06:41:54 +0000

    Added interactive vegalite to hardcoded data

--- hansroslinger/src/hardcodedData.ts
+++ hansroslinger/src/hardcodedData.ts
@@ -3,27 +3,27 @@ import { Uploads } from "types/application";
 
 export const hardcodedUploads: Uploads = {
   "1": {
-    name: "graph1",
-    type: FILE_TYPE_PNG,
-    src: "/uploads/chart-icon.png",
-    thumbnailSrc: "",
-  },
-  "2": {
     name: "ian.png",
     type: FILE_TYPE_PNG,
     src: "/uploads/ian.png",
     thumbnailSrc: "/uploads/ian.png",
   },
+  "2": {
+    name: "Simple line chart",
+    type: FILE_TYPE_JSON,
+    src: "/uploads/line-chart.json",
+    thumbnailSrc: "",
+  },
   "3": {
     name: "Simple bar chart",
     type: FILE_TYPE_JSON,
-    src: "/uploads/example-vega-lite.vg.json",
+    src: "/uploads/bar-chart.json",
     thumbnailSrc: "",
   },
   "4": {
-    name: "pie_chart.png",
-    type: FILE_TYPE_PNG,
-    src: "/uploads/chart-icon.png",
-    thumbnailSrc: "/uploads/chart-icon.png",
+    name: "Simple pie chart",
+    type: FILE_TYPE_JSON,
+    src: "/uploads/pie-chart.json",
+    thumbnailSrc: "",
   },
 };
