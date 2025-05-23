eccd8dbb3d11cf42f881402bd8ad3251dadd4e17
StephSS14 <stephanie.sisilia@gmail.com>
2025-05-23 08:48:52 +0000

    Removed konva canvas

--- hansroslinger/src/components/KonvaOverlay.tsx
+++ hansroslinger/src/components/KonvaOverlay.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useRef, useState } from "react";
-import { Stage, Layer, Rect } from "react-konva";
 import ImageVisual from "./visuals/ImageVisual";
 import { useVisualStore } from "store/visualsSlice";
 import { FILE_TYPE_JSON, FILE_TYPE_PNG } from "constants/application";
@@ -35,29 +34,10 @@ const KonvaOverlay = () => {
     return () => window.removeEventListener("resize", updateSize);
   }, []);
 
-  // Konva js does not allow for html container or DOM element to be rendered directly in the canvas
-  // It does not support DOM element as a child of its layer
-  // So, a transparent rectangle is rendered instead and the VegaLite div component is rendered on top of the canvas
-  // This is the reason why position and size needs to be tracked.
-
   return (
     <div className="absolute inset-0 z-10">
       {dimensions && (
         <div className="w-full h-full pointer-events-auto">
-          <Stage width={dimensions.width} height={dimensions.height}>
-            <Layer>
-              {visuals.map((visual) => (
-                <Rect
-                  key={visual.assetId}
-                  x={visual.position.x}
-                  y={visual.position.y}
-                  width={visual.size.width}
-                  height={visual.size.height}
-                  fill="transparent"
-                />
-              ))}
-            </Layer>
-          </Stage>
           {visuals.map((visual) => {
             if (visual.uploadData.type === FILE_TYPE_JSON) {
               return (
