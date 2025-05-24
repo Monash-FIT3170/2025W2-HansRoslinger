40a0eee00613f5ed0c08b2871ecb82c87353d0dd
StephSS14 <stephanie.sisilia@gmail.com>
2025-05-24 13:50:14 +0000

    Added gesture listener

---
+++ hansroslinger/src/components/interactions/useGestureListener.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { gestureToActionMap } from "./gestureMappings";
+import { InteractionManager } from "./interactionManager";
+import { useGestureStore } from "store/gestureSlice";
+
+export const useGestureListener = (interactionManager: InteractionManager) => {
+  const gesturePayloads = useGestureStore((state) => state.gesturePayloads);
+
+  useEffect(() => {
+    if (!gesturePayloads) return;
+
+    gesturePayloads.forEach((payload) => {
+      const action = gestureToActionMap[payload.name]
+      if (action){
+        // change this to handle action when implemented
+        interactionManager.handleAction({
+          action: action,
+          coordinates: Object.values(payload.points)
+        })
+      }
+    })
+
+  }, [gesturePayloads, interactionManager]);
+
+  return null;
+};
