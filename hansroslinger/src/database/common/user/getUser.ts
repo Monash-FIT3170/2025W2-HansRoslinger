ecff28f9f71533da866d0982ce92879438af16f0
Vedansh Malhan <vmal0014@student.monash.edu>
2025-08-02 23:25:25 +1000

    feat(auth): Linked login screen with database

---
+++ hansroslinger/src/database/common/user/getUser.ts
@@ -0,0 +1,14 @@
+import prisma from "../client";
+
+
+export async function getUser(emailToFind: string) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: emailToFind },
+    });
+    return user;
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    throw error;
+  }
+}
