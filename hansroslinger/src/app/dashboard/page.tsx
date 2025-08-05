879c9fb817ba01a9870e9e3e978b59bdd051f1c3
Vedansh Malhan <vmal0014@student.monash.edu>
2025-08-05 17:22:56 +1000

    feath(auth): Added api for getting a user

--- hansroslinger/src/app/dashboard/page.tsx
+++ hansroslinger/src/app/dashboard/page.tsx
@@ -4,10 +4,14 @@ import UploadsDisplay from "./UploadsDisplay";
 import { Uploads } from "types/application";
 import { hardcodedUploads } from "hardcodedData";
 import Preview from "@/components/Preview";
+import { cookies } from 'next/headers';
 
-const Dashboard = () => {
+const Dashboard = async () => {
   // Right now this is hard coded, once we have data base setup, we can fetch instead
   const uploads: Uploads = hardcodedUploads;
+  const cookieStore = await cookies();
+  const email = cookieStore.get('email')?.value;
+  console.log("Email from cookies:", email);
 
   const gestures = [
     {
