ad519d2814a81e3669ec544421a6591ad21b2d67
StephSS14 <stephanie.sisilia@gmail.com>
2025-08-09 13:28:43 +0000

    hotfix(interaction): Fix and refactor hover to use new hand mapping

--- hansroslinger/src/components/interactions/actions/handleHover.tsx
+++ hansroslinger/src/components/interactions/actions/handleHover.tsx
@@ -1,23 +1,13 @@
 import { useVisualStore } from "store/visualsSlice";
 
-let lastHoveredId: string | null = null;
-
 export const handleHover = (id: string | null, isHovered: boolean) => {
   const store = useVisualStore.getState();
 
-  if (lastHoveredId && lastHoveredId !== id) {
-    store.setVisualHover(lastHoveredId, false);
-  }
-
-  if (id && isHovered) {
+  if (id) {
     const visual = store.getVisual(id);
     if (visual) {
-      store.setVisualHover(id, true);
-      lastHoveredId = id;
+      store.setVisualHover(id, isHovered);
     }
   }
 
-  if (!id) {
-    lastHoveredId = null;
-  }
 };
