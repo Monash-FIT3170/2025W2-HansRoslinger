174e48a8064b5d69957ff067c8e56f99b4d6e292
JoanneYew <joanneliwenyew@gmail.com>
2025-08-17 05:36:44 +0000

    Added feedback to the bottom right of a hovered visual

--- hansroslinger/src/components/interactions/actions/handleFeedback.tsx
+++ hansroslinger/src/components/interactions/actions/handleFeedback.tsx
@@ -43,23 +43,19 @@ export const FeedbackDisplay = ({ fileType }: FeedbackDisplayProps) => {
 
   console.log("Gestures for file type:", fileType, gestures);
   return (
-    <div>
+    <div className="z-20 bg-white bg-opacity-90 rounded-lg shadow-lg p-2">
       {gestures.map((g, idx) => (
         <div key={idx}>
           <div className="flex justify-center items-center gap-2 mb-2">
-            {Array.isArray(g.img) ? (
-              g.img.map((src, i) => (
-                <Image
-                  key={i}
-                  src={src}
-                  alt={`${g.title} ${i + 1}`}
-                  width={16}
-                  height={16}
-                />
-              ))
-            ) : (
-              <Image src={g.img} alt={g.title} width={16} height={16} />
-            )}
+            {(Array.isArray(g.img) ? g.img : [g.img]).map((src, i) => (
+              <Image
+                key={i}
+                src={src}
+                alt={`${g.title} ${i + 1}`}
+                width={16}
+                height={16}
+              />
+            ))}
           </div>
         </div>
       ))}
