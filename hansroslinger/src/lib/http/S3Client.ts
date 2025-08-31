c7e3c06c010c007e594b9d1929f92762544087ca
Rohan <rohanjaggi2003@gmail.com>
2025-08-31 09:03:28 +0000

    chore: Ran prettier and lint

--- hansroslinger/src/lib/http/S3Client.ts
+++ hansroslinger/src/lib/http/S3Client.ts
@@ -9,10 +9,11 @@ if (!AWS_REGION || !AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY) {
 }
 console.log("ENV DEBUG:", {
   AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY ? "Present" : "Missing",
-  AWS_REGION: process.env.AWS_REGION
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY
+    ? "Present"
+    : "Missing",
+  AWS_REGION: process.env.AWS_REGION,
 });
-
 
 export const s3Client = new S3Client({
   region: AWS_REGION,
