ec6663fda82f1110eaa34ada833756292fb7be64
Rohan <rohanjaggi2003@gmail.com>
2025-08-31 09:03:28 +0000

    chore: Ran prettier and lint

--- hansroslinger/src/lib/http/createUserBucket.ts
+++ hansroslinger/src/lib/http/createUserBucket.ts
@@ -1,18 +1,20 @@
-import { PutObjectCommand} from '@aws-sdk/client-s3';
-import { s3Client } from './S3Client';
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { s3Client } from "./S3Client";
 
-export async function createS3UserBucket(user_email: string): Promise<string>{
-    const bucketName = process.env.AWS_BUCKET_NAME
+export async function createS3UserBucket(user_email: string): Promise<string> {
+  const bucketName = process.env.AWS_BUCKET_NAME;
 
-    const s3BucketUrl = `s3://${bucketName}/${user_email}/`;
-    const folderKey = `${user_email}/`;
-        
-    await s3Client.send(new PutObjectCommand({
-        Bucket: bucketName,
-        Key: folderKey,
-        Body: '',
-        ContentType: 'application/x-directory'
-    }));
+  const s3BucketUrl = `s3://${bucketName}/${user_email}/`;
+  const folderKey = `${user_email}/`;
 
-    return s3BucketUrl
+  await s3Client.send(
+    new PutObjectCommand({
+      Bucket: bucketName,
+      Key: folderKey,
+      Body: "",
+      ContentType: "application/x-directory",
+    }),
+  );
+
+  return s3BucketUrl;
 }
