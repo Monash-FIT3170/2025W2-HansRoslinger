295385a4e10bde4d602ed434e437bc6c369775eb
rjaggi3 <rohanjaggi2003@gmail.com>
2025-09-03 09:44:39 +1000

    chore: ran prettier

--- hansroslinger/src/lib/http/deleteFile.ts
+++ hansroslinger/src/lib/http/deleteFile.ts
@@ -1,5 +1,5 @@
 import { DeleteObjectCommand } from "@aws-sdk/client-s3";
-import { s3Client } from "./s3Client";
+import { s3Client } from "./S3Client";
 
 export async function deleteFile(
   userEmail: string,
