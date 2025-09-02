cf95b2b138bc3a3c7fb40053c830842e78f04abb
William Eap <will.techprojects@gmail.com>
2025-09-02 20:43:59 +1000

    fix: fix s3 client setup

--- hansroslinger/src/lib/http/s3Client.ts
+++ hansroslinger/src/lib/http/s3Client.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 const { AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env;
 
+<<<<<<< HEAD
 export const s3Client = new S3Client({
       region: AWS_REGION,
       credentials: {
@@ -11,3 +12,22 @@ export const s3Client = new S3Client({
         secretAccessKey: AWS_SECRET_ACCESS_KEY!,
       },
     });
+=======
+if (!AWS_REGION || !AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY) {
+  throw new Error("Missing AWS credentials or region in .env file");
+}
+console.log("ENV DEBUG:", {
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY ? "Present" : "Missing",
+  AWS_REGION: process.env.AWS_REGION
+});
+
+
+export const s3Client = new S3Client({
+  region: AWS_REGION,
+  credentials: {
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  },
+});
+>>>>>>> 1674550 (fix: fix s3 client setup)
