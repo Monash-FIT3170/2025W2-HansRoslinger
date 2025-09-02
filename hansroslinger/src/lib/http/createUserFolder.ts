8425788951fd1731ca25e38f96e8a28988253a81
hfulto <hfulto303@gmail.com>
2025-09-02 14:44:58 +1000

    chore(pull): update to newest version of develop

---
+++ hansroslinger/src/lib/http/createUserFolder.ts
@@ -0,0 +1,20 @@
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { s3Client } from "./s3Client";
+
+export async function createUserFolder(user_email: string): Promise<string> {
+  const bucketName = process.env.AWS_BUCKET_NAME;
+
+  const s3BucketUrl = `s3://${bucketName}/${user_email}/`;
+  const folderKey = `${user_email}/`;
+
+  await s3Client.send(
+    new PutObjectCommand({
+      Bucket: bucketName,
+      Key: folderKey,
+      Body: "",
+      ContentType: "application/x-directory",
+    }),
+  );
+
+  return s3BucketUrl;
+}
