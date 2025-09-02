6e0d0b6a8c9bb9b7e9b9e3499d8012c950035ebc
rjaggi3 <rohanjaggi2003@gmail.com>
2025-09-03 09:44:39 +1000

    chore: ran prettier

--- hansroslinger/src/lib/http/downloadFile.ts
+++ hansroslinger/src/lib/http/downloadFile.ts
@@ -1,8 +1,8 @@
-import { promises as fs } from 'fs';
-import path from 'path';
-import { pipeline } from 'stream';
-import { promisify } from 'util';
-import { getObject } from './getObject';
+import { promises as fs } from "fs";
+import path from "path";
+import { pipeline } from "stream";
+import { promisify } from "util";
+import { getObject } from "./getObject";
 
 const streamPipeline = promisify(pipeline);
 
@@ -13,16 +13,15 @@ export async function downloadFile(
 ): Promise<string> {
   const { body, s3Key } = await getObject(userEmail, keyOrFileName);
 
-  if (!body || typeof (body as any).pipe !== 'function') {
+  if (!body || typeof (body as any).pipe !== "function") {
     throw new Error(`Unexpected Body type for key: ${s3Key}`);
   }
 
   await fs.mkdir(downloadDir, { recursive: true });
   const outputFilePath = path.join(downloadDir, path.basename(s3Key));
 
-  const nodeFs = await import('fs');
+  const nodeFs = await import("fs");
   await streamPipeline(body as any, nodeFs.createWriteStream(outputFilePath));
 
   return outputFilePath;
 }
-
