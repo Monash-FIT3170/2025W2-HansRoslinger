d3d04cc836807feeaa6d480c2ef64d57ffe1c9a9
William Eap <will.techprojects@gmail.com>
2025-09-02 20:43:38 +1000

    feat: function to retrieve file from s3 bucket

--- hansroslinger/src/lib/http/retrieveFiles.ts
+++ hansroslinger/src/lib/http/retrieveFiles.ts
@@ -1,31 +1,30 @@
-import { ListObjectsV2Command } from "@aws-sdk/client-s3";
-import { s3Client } from "./s3Client";
-import type { FileObject } from "./fileInterfaces";
+import { ListObjectsV2Command } from '@aws-sdk/client-s3';
+import { s3Client } from './S3Client';
+import type { FileObject } from './fileInterfaces';
 
-export async function retrieveUserFiles(
-  userEmail: string,
-): Promise<FileObject[]> {
+export async function retrieveUserFiles(userEmail: string): Promise<FileObject[]> {
+    
   try {
     const bucketName = process.env.AWS_BUCKET_NAME;
 
     const command = new ListObjectsV2Command({
       Bucket: bucketName,
-      Prefix: `${userEmail}/`,
+      Prefix:  `${userEmail}/`,
       MaxKeys: 1000,
     });
 
     const response = await s3Client.send(command);
     const files = response.Contents || [];
 
-    return files.map((file) => ({
+    return files.map(file => ({
       key: file.Key!,
       size: file.Size!,
       lastModified: file.LastModified!,
       etag: file.ETag!,
-      url: `s3://${bucketName}/${file.Key}`,
+      url: `s3://${bucketName}/${file.Key}`
     }));
   } catch (error) {
-    console.error("Error retrieving user files:", error);
+    console.error('Error retrieving user files:', error);
     throw error;
   }
 }
