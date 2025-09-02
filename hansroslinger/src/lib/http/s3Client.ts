f42d064a278e375901527ffaf874ab084950399c
Rohan <rohanjaggi2003@gmail.com>
2025-09-02 01:12:34 +0000

    feat(dataStorage): Added functions to create a folder for a user in s3 bucket

---
+++ hansroslinger/src/lib/http/s3Client.ts
@@ -0,0 +1,24 @@
+import { S3Client } from "@aws-sdk/client-s3";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env;
+
+if (!AWS_REGION || !AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY) {
+  throw new Error("Missing AWS credentials or region in .env file");
+}
+console.log("ENV DEBUG:", {
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY
+    ? "Present"
+    : "Missing",
+  AWS_REGION: process.env.AWS_REGION,
+});
+
+export const s3Client = new S3Client({
+  region: AWS_REGION,
+  credentials: {
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  },
+});
