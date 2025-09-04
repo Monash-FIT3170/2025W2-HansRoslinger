4bf7b6517a94a8433a8239504ba9df483581e5a2
Vedansh Malhan <vedanshmalhan03@gmail.com>
2025-09-04 22:59:52 +1000

    hotfix(signup-page) added vercel.json for routing and made error logging better on the signup page

--- hansroslinger/src/app/signup/page.tsx
+++ hansroslinger/src/app/signup/page.tsx
@@ -37,18 +37,22 @@ export default function SignUpPage() {
       const data: SignupResponse = await res.json();
 
       if (!data.user) {
-        setError(data.error || "Login failed");
+        console.log(data.error);
+        if (data.error == "\nInvalid `prisma.user.create()` invocation:\n\n\nUnique constraint failed on the fields: (`email`)") {
+          setError("User already exists with this email.");
+        } else {
+          setError(data.error || "Signup failed");
+        }
         return;
       }
       redirect("/login");
     } catch (error) {
       setError(
-        "Error creating user. Please try again. User may already exist.",
+        "Error creating user. Please try again. If the problem persists, contact support.",
       );
-      console.error("Error creating user:", error);
+      console.log("Error creating user:", error);
       return;
     }
-    redirect("/login");
   };
 
   return (
