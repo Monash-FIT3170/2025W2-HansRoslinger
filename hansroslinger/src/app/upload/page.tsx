d81640ccf54fa36d67c206c81c4512de2198554a
Dana McDonald <danatrmcdonald@gmail.com>
2025-09-05 13:20:32 +1000

    Remove whitespace

--- hansroslinger/src/app/upload/page.tsx
+++ hansroslinger/src/app/upload/page.tsx
@@ -197,7 +197,7 @@ export default function UploadPage() {
                 </div>
               ))}
             </div>
-            
+
             <div className="mt-6 flex justify-end">
               <button
                 onClick={handleUpload}
