c1ecdfa57b9c468ae2ddd24d9241ee7057ac719d
Rohan <rohanjaggi2003@gmail.com>
2025-09-14 04:06:33 +0000

    hotfix(login): Commented out router refresh for debugging

--- hansroslinger/src/app/login/page.tsx
+++ hansroslinger/src/app/login/page.tsx
@@ -34,8 +34,8 @@ export default function LoginPage() {
       setError(data.error || "Login failed");
       return;
     }
-    await router.refresh()
-    await router.push("/dashboard");
+    //router.refresh()
+    router.push("/dashboard");
   };
 
   return (
