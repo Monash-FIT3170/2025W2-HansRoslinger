0eae73f0a8a71bf11c77727463656080f994d3f1
Rohan <rohanjaggi2003@gmail.com>
2025-09-14 05:02:40 +0000

    hotfix(login): Changed middleware config so it runs on the login and singup

--- hansroslinger/src/middleware.ts
+++ hansroslinger/src/middleware.ts
@@ -21,5 +21,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next|.*\\..*|favicon.ico|login|signup).*)"],
+  matcher: ["/((?!api|_next|.*\\..*|favicon.ico).*)"],
 };
