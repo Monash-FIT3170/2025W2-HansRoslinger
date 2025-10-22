f98c687f9e34a9e56b3bbb7625183c1885920ecc
hfulto <143306419+hfulto@users.noreply.github.com>
2025-10-22 15:53:44 +1000

    Revert "bug fixes and giving option to upload an asset to a collection"
    This reverts commit 5c7c2f3f39e5d1bbbc0deefc5d39bd243a5f4785.

--- hansroslinger/src/database/common/collections/createAsset.ts
+++ hansroslinger/src/database/common/collections/createAsset.ts
@@ -2,25 +2,31 @@ import prisma from "../client";
 import { getCollection } from "./getCollection";
 
 export async function createAsset(
-  collectionID : number,
+  collectionName: string,
   name: string,
+  email: string
 ) {
   try {
+    const collection = await getCollection(collectionName, email);
+    if (!collection) {
+      throw new Error(`Collection with name ${collectionName} for user ${email} not found`);
+    }
+
     const asset = await prisma.asset.create({
       data: {
         name: name,
-        collectionID: collectionID
+        collectionID: collection.id
       },    
       select: {
         id: true
       }
     });
     await prisma.collection.update({
-      where: { id: collectionID },
+      where: { id: collection.id },
       data: { assets: { connect: { id: asset.id } } }
     });
 
-    console.log(`Asset for collection ${collectionID} created:`, asset.id);
+    console.log(`Asset for collection ${collection.id} created:`, asset.id);
     return asset;
   } catch (error) {
     console.error("Error creating asset:", error);
