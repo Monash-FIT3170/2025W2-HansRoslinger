7fc4934457c7f66a920392f4cd3d369c23129c43
Rohan <rohanjaggi2003@gmail.com>
2025-10-23 08:03:32 +0000

    feat(collections): Added code to create, retrieve and select a collection.

--- hansroslinger/src/database/common/collections/getAsset.ts
+++ hansroslinger/src/database/common/collections/getAsset.ts
@@ -1,11 +1,11 @@
 import prisma from "../client";
 import { getCollection } from "./getCollection";
 
-export async function getAsset(assetName: string, collectionName: string, email: string) {
+export async function getAsset(assetName: string, collectionName: string, userID: number) {
   try {
-    const foundCollection = await getCollection(collectionName, email);
+    const foundCollection = await getCollection(collectionName, userID);
     if (!foundCollection) {
-      throw new Error(`Collection with name ${collectionName} not found for user ${email}`);
+      throw new Error(`Collection with name ${collectionName} not found for user ${userID}`);
     }
     const asset = await prisma.asset.findUnique({
       where: { collectionID_name: {name: assetName, collectionID: foundCollection.id }},
